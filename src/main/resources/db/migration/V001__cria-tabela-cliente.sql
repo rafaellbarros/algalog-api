47bcd6615ad718c76913ba86ee00c8dbd8c4f0c0
Rafael <rafaelbarros.df@gmail.com>
2022-05-18 13:51:56 -0300

    :construction: Configurando o Flyway

---
+++ src/main/resources/db/migration/V001__cria-tabela-cliente.sql
@@ -0,0 +1,10 @@
+--
+-- DDL for table cliente
+--
+CREATE TABLE cliente (
+    id  BIGINT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(60) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    telefone VARCHAR(20) NOT NULL,
+    PRIMARY KEY (id)
+);
